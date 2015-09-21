1ae33c025f8e3614c4f599eaeb526a411aaa0e85
Sorina Mancas <sorinam@yahoo.com>
2015-09-21 15:33:52 +0300

    add the fourth test-TestSquareImproperDimension

---
+++ UnitTest4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestSquareImproperDimension()
+        {
+            float lung_p = 0;
+            float lat_p = 7;
+            float a = 4;
+            int expected_result = 0;
+            Calculate classCalculate = new Calculate();
+            int result = classCalculate.CalculDale(lung_p, lat_p, a);
+            Assert.AreEqual(expected_result, result);
+
+        }
+    }
+}
