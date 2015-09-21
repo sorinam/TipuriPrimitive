6726a2c899c4914569a8455543f2169a54df5f9b
Sorina Mancas <sorinam@yahoo.com>
2015-09-21 15:31:50 +0300

    add thirs test- TestDalaImproperDimension

---
+++ UnitTest3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestDalaImproperDimensions()
+        {
+            float lung_p = 1;
+            float lat_p = 7;
+            float a = 0;
+            int expected_result = -1;
+            Calculate classCalculate = new Calculate();
+            int result = classCalculate.CalculDale(lung_p, lat_p, a);
+            Assert.AreEqual(expected_result, result);
+        }
+    }
+}
