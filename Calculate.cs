a0c349b1f92dd545d405d95b3818d3c9fb941003
Sorina Mancas <sorinam@yahoo.com>
2015-09-21 15:27:29 +0300

    add Class Calculate; add first test- SimpleCase

---
+++ Calculate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriPrimitive
+{
+    class Calculate
+    {
+        public int CalculDale(float m, float n, float a)
+        {
+            int result;
+            if (a > 0)
+            {
+                if (m <= 0)
+                {
+                    //length of square is zero
+                    result = 0;
+                }
+                else
+                {
+                    if (n <= 0)
+                    {
+                        //high of square is zero
+                        result = 0;
+                    }
+                    else
+                    {
+                        var lung = Math.Ceiling(m / a);
+                        var lat = Math.Ceiling(n / a);
+                        result = (int)(lung * lat);
+                    }
+                }
+
+            }
+            else
+            {
+                //Dimesion of dala =0
+                result = -1;
+            }
+            return result;
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,9 +7,16 @@ namespace TipuriPrimitive
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleCase()
         {
-            // first change
+            // length%a = 0 and high%a=0
+            int lung_p = 6;
+            int lat_p = 8;
+            int a = 2;
+            int expected_result = 12;
+            Calculate classCalculate = new Calculate();
+            int result = classCalculate.CalculDale(lung_p, lat_p, a);
+            Assert.AreEqual(expected_result, result);
         }
     }
 }
