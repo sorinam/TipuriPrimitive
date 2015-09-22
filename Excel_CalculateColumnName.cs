eb9a1bc031b716f60b7165dd830946d805916bfb
Sorina Mancas <sorinam@yahoo.com>
2015-09-22 14:40:26 +0300

    add test for more letters

--- Excel_CalculateColumnName.cs
+++ Excel_CalculateColumnName.cs
@@ -10,25 +10,33 @@ namespace TipuriPrimitive
     {
         public string ColumnName(int column)
         {
+            string def_results = "";
             string calc_result = "";
             if (column > 0)
             {//valid column number
                 int number = column;
                 int Cat;
                 int rest;
-                Cat = (number / 26);
-                rest = number % 26;
-                if ((rest) == 0)
-                { number = (Cat - 1); }
-                else
-                { number = Cat; }
-                calc_result += ConvertNumberToLetter(rest);
+                do
+                {
+                    Cat = (number / 26);
+                    rest = number % 26;
+                    if ((rest) == 0)
+                    { number = (Cat - 1); }
+                    else
+                    { number = Cat; }
+                    calc_result += ConvertNumberToLetter(rest);
+                } while (number > 0);
+
+                for (int i = calc_result.Length - 1; i >= 0; i--)
+                { def_results += calc_result[i]; }
             }
+        
             else
             {//invalid column number -less or equal 0
-                calc_result += "-1";
+                def_results += "-1";
             }
-                return calc_result;
+                return def_results;
         }
         public char ConvertNumberToLetter(int number)
         {
--- Excel_TestMoreLetters.cs
+++ Excel_TestMoreLetters.cs
@@ -4,13 +4,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TipuriPrimitive
 {
     [TestClass]
-    public class UnitTest5
+    public class Excel_TestMoreLetters
     {
         [TestMethod]
-        public void Excel_TestOneLetterDiffZ()
+        public void Excel_TestsMoreLetters()
         {
-            int column_number = 1;
-            string expected_result = "A";
+            int column_number = 18280;
+            string expected_result = "AAAB";
             Excel_CalculateColumnName calculateColumnName = new Excel_CalculateColumnName();
             string result = calculateColumnName.ColumnName(column_number);
             Assert.AreEqual(expected_result, result);
