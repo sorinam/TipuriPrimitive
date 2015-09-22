06ccdb729aa26c0460d71403d0e8eddf7bbe5e14
Sorina Mancas <sorinam@yahoo.com>
2015-09-22 22:06:19 +0300

    add CalculateBirdDistance test

---
+++ Train_CalcBirdDist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class Train_CalcBirdDist
+    {
+        [TestMethod]
+        public void Train_CalculateBirdDistance()
+        {
+            float X = 3;
+            float D = 10;
+            CalcBirdDistance calcBirdDistance = new CalcBirdDistance();
+            float result = 0;
+            float expected_result = 10;
+            result = calcBirdDistance.CalculateBirdDistance(D, X);
+            Assert.AreEqual(expected_result, result);
+        }
+    }
+}
--- Train_CalcBirdDistance.cs
+++ Train_CalcBirdDistance.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 namespace TipuriPrimitive
 {
     class CalcBirdDistance
-    {public float CalculateBirdDistance(int D, int X)
+    {public float CalculateBirdDistance(float D, float X)
         {
             float result=0 ;
             if ((D==0)||(X==0))
-                    {//invalid data
+                {//invalid data
                 result = -1;
+                }
+            else
+            {//correct data
+             float time = D / ( 2 * X);
+             float bird_distance = 2 * X * time;
+             result = bird_distance;
             }
             return result;
         }
