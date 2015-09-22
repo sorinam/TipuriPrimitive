efd021fa583acd94671af5a836402c02f292d0e0
Sorina Mancas <sorinam@yahoo.com>
2015-09-22 18:59:32 +0300

    add Mushrooms clas and test invalid input

---
+++ Mushrooms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriPrimitive
+{
+    class Mushrooms
+    {
+        public int CalculateWhiteMushrroms(int N,int X)
+        {
+            int result = 0;
+            if (X>N)
+            {//no solution
+                result = -1;
+            }
+            return result;
+
+        }
+    }
+}
---
+++ Mushrooms_NoSolution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class Mushrooms_NoSolution
+    {
+        [TestMethod]
+        public void Mushrooms_InvalidData()
+        {
+            int N = 10;
+            int X = 11;
+            Mushrooms mushrooms = new Mushrooms();
+            int white_mushrooms = 0;
+            int expected_whitemushrooms = -1;
+            white_mushrooms = mushrooms.CalculateWhiteMushrroms(N, X);
+            Assert.AreEqual(expected_whitemushrooms, white_mushrooms);
+        }
+    }
+}
