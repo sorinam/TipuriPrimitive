201c0cfb5ddf1ca4427941c6d9fdf3c5982e6041
Sorina Mancas <sorinam@yahoo.com>
2015-09-22 21:48:54 +0300

    add class an first Train test

---
+++ Train_CalcBirdDistance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriPrimitive
+{
+    class CalcBirdDistance
+    {public float CalculateBirdDistance(int D, int X)
+        {
+            float result=0 ;
+            if ((D==0)||(X==0))
+                    {//invalid data
+                result = -1;
+            }
+            return result;
+        }
+    }
+}
---
+++ Train_InvalidData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class Train_InvalidData
+    {
+        [TestMethod]
+        public void Train_ImproperInputData()
+        {
+            int X = 0;
+            int D = 10;
+            CalcBirdDistance calcBirdDistance = new CalcBirdDistance();
+            float result = 0;
+            float expected_result = -1;
+            result = calcBirdDistance.CalculateBirdDistance(D,X);
+            Assert.AreEqual(expected_result,result);
+        }
+    }
+}
