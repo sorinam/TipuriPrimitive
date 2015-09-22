be7a2c85d124d1996762ecd5a120683b452ff3a7
Sorina Mancas <sorinam@yahoo.com>
2015-09-22 18:12:15 +0300

    add 1DayYGoats test

---
+++ Capre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriPrimitive
+{
+    class Goats
+    {
+        public float CalculateCant(int Xdays, int Ygoats, float Zkg, int Wdays, int Qgoats)
+        {
+            float result;
+            result = Zkg / Xdays;
+            return result;
+        }
+    }
+}
---
+++ OneDayYGoats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class OneDayYGoats
+    {
+        [TestMethod]
+        public void O1DayYGoats()
+        {
+            int Xdays = 5;
+            int Ygoats = 3;
+            int Zkg = 10;
+            int Wdays = 1;
+            int Qgoats = 9;
+            float result = 0;
+            Goats goats = new Goats();
+            float expected_result =2;
+            result = goats.CalculateCant(Xdays, Ygoats, Zkg, Wdays, Qgoats);
+            Assert.AreEqual(expected_result, result);
+
+        }
+    }
+}
