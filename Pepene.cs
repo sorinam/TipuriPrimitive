06a082b7ec6707b14b016c28030a6b6634a3fa2c
Sorina Mancas <sorinam@yahoo.com>
2015-09-22 16:05:16 +0300

    add test Pepene_DA

---
+++ Pepene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriPrimitive
+{
+    class Pepene
+    {
+        public string Verify_Kg(int i)
+        {
+            string result = "";
+                if (i % 2 == 0)
+            {
+                result = "DA";
+            }
+         return result;
+        }
+    }
+}
---
+++ Pepene_TestDA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class Pepene_TestDA
+    {
+        [TestMethod]
+        public void Pepene_DA()
+        {
+            Pepene pepene = new Pepene();
+            int x = 8;
+            string expected_result = "DA";
+            string result = pepene.Verify_Kg(x);
+            Assert.AreEqual(expected_result, result);
+
+        }
+    }
+}
