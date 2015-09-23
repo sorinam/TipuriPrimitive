92892135a9d080a215b2562a015631ab54aa24d6
Sorina Mancas <sorinam@yahoo.com>
2015-09-23 17:39:33 +0300

    add Bank_Calculate rate from N month

---
+++ Bank_NRate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class Bank_NRate
+    {
+        [TestMethod]
+        public void Bank_CalculateRateN()
+        {
+            Bank_CalculateRate bank_CalculateRate = new Bank_CalculateRate();
+            //initial sum
+            int sum = 40000;
+            //period
+            int period = 20;
+            //interest
+            float anual_interest = 7.57f;
+            //interested monthly number =39 march in the 4 year
+            int monthly_number = 39;
+            float result_rate = 0;
+            result_rate = bank_CalculateRate.MonthlyBankRate(sum, period, anual_interest, monthly_number);
+            float expected_rate = 379.0472f;
+            Assert.AreEqual(expected_rate.ToString(), result_rate.ToString());
+        }
+    }
+}
