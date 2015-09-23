d04574dc9b7d838f4b9fa3b63421cccdb747eb79
Sorina Mancas <sorinam@yahoo.com>
2015-09-23 17:14:01 +0300

    add invalid monthly_number test

--- Bank_CalculateRate.cs
+++ Bank_CalculateRate.cs
@@ -9,16 +9,24 @@ namespace TipuriPrimitive
     class Bank_CalculateRate
     {
         public float MonthlyBankRate(float sum, int period, float anual_interest, int monthly_number)
-        {//calculate pricipal
-            float principal = sum / (period * 12);
-            //calculate lunar interest
-            float lunar_interest = anual_interest / 12/100;
-            //calculate amount
-            float amount = sum - (monthly_number - 1) * principal;
-            //calculate interest of amount
-            float interest = lunar_interest * amount;
-            //calculate Rate
-            float rate = principal +interest;
+        {
+            float rate = 0;
+            if(monthly_number<1 || monthly_number>period*12)
+            {//invalid monthly number, it can't be negative or grater than period of contract
+                rate = -1;
+            }
+                else
+            {//correct monyhly_number
+                //calculate pricipal
+                float principal = sum / (period * 12);
+                //calculate lunar interest
+                float lunar_interest = anual_interest / 12 / 100;
+                //calculate amount
+                float amount = sum - (monthly_number - 1) * principal;
+                //calculate interest of amount
+                float interest = lunar_interest * amount;
+                //calculate Rate
+                rate = principal + interest; }
             return rate;
         }
     }
---
+++ Bank_InvalidMonthNb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class Bank_InvalidMonthNb
+    {
+        [TestMethod]
+        public void Bank_InvalidMonthlyNumber()
+        {
+            Bank_CalculateRate bank_CalculateRate = new Bank_CalculateRate();
+            //initial sum
+            int sum = 40000;
+            //period
+            int period = 20;
+            //interest
+            float anual_interest = 7.57f;
+            //interested monthly number
+            int monthly_number = 1000;
+            float result_rate = 0;
+            result_rate = bank_CalculateRate.MonthlyBankRate(sum, period, anual_interest, monthly_number);
+            float expected_rate =-1;
+            Assert.AreEqual(expected_rate, result_rate);
+        }
+    }
+}
