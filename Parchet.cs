2b6a930e7e0c4964b8378d479b5e53163feafab4
Sorina Mancas <sorinam@yahoo.com>
2015-09-23 19:17:50 +0300

    add first Parchet test

---
+++ Parchet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class Parchet
+    {
+        [TestMethod]
+        public void Parchet_NumberofPieces()
+        {//room dimension
+            float M = 10;
+            float N = 3;
+            //parquet dimensions
+            float A = 0.7f;
+            float B = 0.15f;
+            //extra
+            float extra = 0.15f;
+            Parchet_pieces parchet = new Parchet_pieces();
+            int result = parchet.CalculatenumberofPieces(M,N,A,B,extra);
+            int expected_result = 34;
+            Assert.AreEqual(expected_result, result);
+
+        }
+    }
+}
---
+++ Parchet_pieces.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriPrimitive
+{
+    class Parchet_pieces
+    {
+        public int CalculatenumberofPieces(float M, float N, float A, float B, float extra)
+        {
+            int result;
+            float calculate;
+            float area1;
+            //calculate the area of the room
+            area1 = M * N;
+            //calculate de area of a pieces of parquet
+            float area2 = A * B;
+            //calculate number of pieces
+            calculate = area1 / area2;
+            //put extra needed
+            calculate = calculate * extra;
+            result = (int)(Math.Ceiling(calculate));
+            return result;
+
+        }
+    }
+}
