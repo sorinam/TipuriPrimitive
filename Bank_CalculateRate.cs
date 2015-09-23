30bbe2081ebbed78712f7ff4d245ed3202b9ba8c
Sorina Mancas <sorinam@yahoo.com>
2015-09-23 13:54:37 +0300

    add class and  Bank_FirstRate test

---
+++ Bank_CalculateRate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriPrimitive
+{
+    class Bank_CalculateRate
+    {
+        public float MonthlyBankRate(float sum, int period, float anual_interest, int monthly_number)
+        {//calculate pricipal
+            float principal = sum / (period * 12);
+            //calculate lunar interest
+            float lunar_interest = anual_interest / 12/100;
+            //calculate amount
+            float amount = sum - (monthly_number - 1) * principal;
+            //calculate interest of amount
+            float interest = lunar_interest * amount;
+            //calculate Rate
+            float rate = principal +interest;
+            return rate;
+        }
+    }
+}
