fe9135ad5ba74290d6d4bc6d0fdfdd131087f8be
Sorina Mancas <sorinam@yahoo.com>
2015-09-24 11:49:20 +0300

    add Parchet_InvalidDimensions test

---
+++ ParchetInvalidDim.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class ParchetInvalidDim
+    {
+        [TestMethod]
+        public void Parchet_InvalidDimesnions()
+        {//room dimension
+            float M =0;
+            float N = 3;
+            //parquet dimensions
+            float A = 0;
+            float B = 0.15f;
+            //extra
+            float extra = 0.15f;
+            Parchet_pieces parchet = new Parchet_pieces();
+            int result = parchet.CalculatenumberofPieces(M, N, A, B, extra);
+            int expected_result = -1;
+            Assert.AreEqual(expected_result, result);
+        }
+    }
+}
--- Parchet_pieces.cs
+++ Parchet_pieces.cs
@@ -11,17 +11,31 @@ namespace TipuriPrimitive
         public int CalculatenumberofPieces(float M, float N, float A, float B, float extra)
         {
             int result;
-            float calculate;
-            float area1;
-            //calculate the area of the room
-            area1 = M * N;
-            //calculate de area of a pieces of parquet
-            float area2 = A * B;
-            //calculate number of pieces
-            calculate = area1 / area2;
-            //put extra needed
-            calculate = calculate * (1+extra);
-            result = (int)(Math.Ceiling(calculate));
+            if (M * N * A * B != 0)
+            {if (M > 0 && N > 0 && A > 0 && B > 0)
+                {
+                    //valid dimensions
+                    float calculate;
+                    float area1;
+                    //calculate the area of the room
+                    area1 = M * N;
+                    //calculate de area of a pieces of parquet
+                    float area2 = A * B;
+                    //calculate number of pieces
+                    calculate = area1 / area2;
+                    //put extra needed
+                    calculate = calculate * (1 + extra);
+                    result = (int)(Math.Ceiling(calculate));
+                }
+            else
+                {//invalid dimensions; one zise is negative
+                    result = -1;
+                }
+            }
+            else
+            {//invalid dimesions, one size equal to 0
+                result = -1;
+            }
             return result;
 
         }
