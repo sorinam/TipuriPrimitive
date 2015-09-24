07e468d189495188d8259dcf728832f2c648979a
Sorina Mancas <sorinam@yahoo.com>
2015-09-24 14:11:48 +0300

    add Columns_CalculateBuildArea test and class

---
+++ Columns_BuildArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+	[TestClass]
+	public class Columns_BuildArea
+	{
+		[TestMethod]
+		public void Columns_CalculateBuildArea()
+        {
+            float xa = 0.000000f;
+            float ya = 0.000000f;
+            float xb = 0.000000f;
+            float yb = 1.000000f;
+            float xc = 1.000000f;
+            float yc = 1.000000f;
+            Columns_CalculateArea column = new Columns_CalculateArea();
+            float result = 0;
+            //the area of build is double of triangles's area
+            result =column.Calculate_AreaofTriangle(xa,ya,xb,yb,xc,yc)*2;
+            float expected_result = 1;
+            Assert.AreEqual(expected_result,result);
+		}
+	}
+}
---
+++ Columns_CalculateArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriPrimitive
+{
+    class Columns_CalculateArea
+    {
+        public float Calculate_AreaofTriangle(float xa, float ya, float xb, float yb, float xc, float yc)
+        { float result = 0;
+            float area = Math.Abs(xa * yb + xb * yc + ya * xc - xc * yb - xa * yc - xb * ya);
+            result = area/2;
+            return result;
+        }
+    }
+}
