6b0eaf6d00457d852ad80ca6b024eb62ca10daf0
Sorina Mancas <sorinam@yahoo.com>
2015-10-02 16:54:15 +0300

    add first test for Lotto

---
+++ Loto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+    [TestClass]
+    public class Loto
+    {
+        [TestMethod]
+        public void SixNumbers()
+        {
+            ulong TotalNumbers = 49;
+            ulong Numbers = 6;
+            ulong ExpectedChanse = 566684;
+            Utils utils= new Utils();
+            Assert.AreEqual(ExpectedChanse,utils.ChancetoWin(TotalNumbers, Numbers));
+          
+        }
+    }
+}
