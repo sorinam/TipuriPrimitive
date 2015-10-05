96965eb723a066b68dd8ea15555cbaec19dfdfe3
Sorina Mancas <sorinam@yahoo.com>
2015-10-05 15:23:02 +0300

    add first version of Encrypt test

--- EncryptDecrypt.cs
+++ EncryptDecrypt.cs
@@ -9,46 +9,77 @@ namespace TipuriPrimitive
         [TestMethod]
         public void EncryptText()
         {
-            string InputString = "nicaieri nu e ca acasa";
+            string inputString = "nicaieri nu e ca acasa";
+            string[] outputString = { "jeea","ircs","ciaa","anaj","iuch" };
             uint NumberOfColumns = 4;
-            string[] ExpectedOutputString = { "neea", "ircs", "ciaa", "ana", "iuc" };
-            Assert.AreEqual(ExpectedOutputString, Encrypt(InputString, NumberOfColumns)); }
+            string[] resultOfEncryption = new string[NumberOfColumns];
+            resultOfEncryption = Encrypt(inputString, NumberOfColumns);
+             for (int j=0;j<NumberOfColumns;j++)
+             {
+                bool testResult = (outputString[j] == resultOfEncryption[j]) || (outputString[j].Contains(resultOfEncryption[j].Substring(0, (int)NumberOfColumns - 1)));
+                Assert.IsTrue(testResult,"Expected: "+ outputString[j]+" Returned:"+ resultOfEncryption[j]);
+             }         
+          }
 
         private string[] Encrypt(string inputString, uint numberOfColumns)
         {
             string textWithoutWhiteSpaces = TrimAndFindWhiteSpaces(inputString);
             int newLength = textWithoutWhiteSpaces.Length;
             uint numberOfRows = CalculateNumberOfRows(newLength, numberOfColumns);
-            char[,] matrix = new char[numberOfRows-1, numberOfColumns-1];
-            int l = 0;
-            int lastrow = 0;
-            for (int j=0;j<numberOfColumns; j++)
-            {
-                for(int i=0;i<numberOfRows;i++)
-                {
-                    matrix[i, j] = textWithoutWhiteSpaces[l];
-                    l++; 
-                    lastrow = i;
-                }
+            char[,] matrixOfChars = FillMatrixWithChars(textWithoutWhiteSpaces, numberOfRows, numberOfColumns);
+            string[] OutputString = GetStringsFromMatrix(matrixOfChars, numberOfRows, numberOfColumns);
+            return OutputString;
             
-            }
-            //fill with random chars;
-            for (int newi= lastrow;newi<numberOfRows;newi++)
-            {
-                matrix[newi, numberOfColumns] = GetRandomLetter();
-            }
+            
+        }
+
+        private string[] GetStringsFromMatrix(char[,] matrixOfChars, uint numberOfRows, uint numberOfColumns)
+        {
             //create output string - read matrix from rows
             string[] OutputString = new string[numberOfRows];
-            for (int i=0;i<numberOfRows;i++)
+            for (int i = 0; i < numberOfRows; i++)
             {
                 OutputString[i] = "";
-                for (int j=0;j<numberOfColumns;j++)
+                for (int j = 0; j < numberOfColumns; j++)
                 {
-                    OutputString[i] += matrix[i, j];
+                    OutputString[i] += matrixOfChars[i, j];
                 }
-             }
+            }
             return OutputString;
         }
+
+        private char[,] FillMatrixWithChars(string textWithoutWhiteSpaces, uint numberOfRows, uint numberOfColumns)
+        {
+            char[,] matrix = new char[numberOfRows, numberOfColumns];
+            int l = 0;
+            int lastrow = 0;
+            
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                for (int i = 0; i < numberOfRows; i++)
+                {
+                    if (l < textWithoutWhiteSpaces.Length)
+                    {
+                        matrix[i, j] = textWithoutWhiteSpaces[l];
+                        l++;
+                     }
+                    else
+                    {
+                        lastrow = i;
+                        i = (int)numberOfRows;
+                    }
+                }
+
+                }
+            //fill with random chars;
+            for (int newi = lastrow; newi < numberOfRows; newi++)
+            {
+                matrix[newi, numberOfColumns-1] = GetRandomLetter();
+            }
+            return matrix;
+            
+        }
+
         private static char GetRandomLetter()
         {   Random random = new Random();
             int num = random.Next(0, 26); 
