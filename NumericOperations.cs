ad30949be931e03e5d3427bc8d8ff2aa300a3170
Sorina Mancas <sorinam@yahoo.com>
2015-10-08 23:28:35 +0300

    add tests for OR, XOR and NOT

--- NumericOperations.cs
+++ NumericOperations.cs
@@ -121,7 +121,7 @@ namespace TipuriPrimitive
         }
 
         [TestMethod]
-        public void AndForLists()
+        public void ANDForListsWithDifferentLength()
         {
             List<byte> List1 = new List<byte>();
             List1.Add(1);
@@ -160,7 +160,7 @@ namespace TipuriPrimitive
             resultList.Add(0);
             resultList.Add(1);
 
-            Assert.IsTrue(IsListValid(List1),"Invalid List1");
+            Assert.IsTrue(IsListValid(List1), "Invalid List1");
             Assert.IsTrue(IsListValid(List2), "Invalid List2");
 
             List<byte> calculateList = List1AndList2(List1, List2);
@@ -168,7 +168,212 @@ namespace TipuriPrimitive
             Assert.IsTrue(AreEqualLists(resultList, calculateList));
         }
 
-        private bool IsListValid(List<byte> listi)
+        [TestMethod]
+        public void ANDForListsWithTheSameLength()
+        {
+            List<byte> List1 = new List<byte>();
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(1);
+
+            List<byte> List2 = new List<byte>();
+            List2.Add(1);
+            List2.Add(1);
+            List2.Add(1);
+            List2.Add(0);
+
+            List<byte> resultList = new List<byte>();
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(0);
+
+            Assert.IsTrue(IsListValid(List1), "Invalid List1");
+            Assert.IsTrue(IsListValid(List2), "Invalid List2");
+
+            List<byte> calculateList = List1AndList2(List1, List2);
+
+            Assert.IsTrue(AreEqualLists(resultList, calculateList));
+        }
+
+        [TestMethod]
+        public void ORForListsWithDifferentLength()
+        {
+            List<byte> List1 = new List<byte>();
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(1);
+
+            List<byte> List2 = new List<byte>();
+            List2.Add(0);
+            List2.Add(1);
+            List2.Add(1);
+            List2.Add(0);
+            List2.Add(1);
+            List2.Add(0);
+            List2.Add(1);
+
+            List<byte> resultList = new List<byte>();
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(0);
+            resultList.Add(1);
+            resultList.Add(0);
+            resultList.Add(1);
+
+            Assert.IsTrue(IsListValid(List1), "Invalid List1");
+            Assert.IsTrue(IsListValid(List2), "Invalid List2");
+
+            List<byte> calculateList = List1OrList2(List1, List2);
+
+            Assert.IsTrue(AreEqualLists(resultList, calculateList));
+        }
+
+        [TestMethod]
+        public void ORForListsWithTheSameLength()
+        {
+            List<byte> List1 = new List<byte>();
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(0);
+
+            List<byte> List2 = new List<byte>();
+            List2.Add(0);
+            List2.Add(1);
+            List2.Add(1);
+            List2.Add(0);
+
+            List<byte> resultList = new List<byte>();
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(0);
+
+            Assert.IsTrue(IsListValid(List1), "Invalid List1");
+            Assert.IsTrue(IsListValid(List2), "Invalid List2");
+
+            List<byte> calculateList = List1OrList2(List1, List2);
+
+            Assert.IsTrue(AreEqualLists(resultList, calculateList));
+        }
+
+        [TestMethod]
+        public void XORForListsWithDifferentLength()
+        {
+            List<byte> List1 = new List<byte>();
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(1);
+
+            List<byte> List2 = new List<byte>();
+            List2.Add(0);
+            List2.Add(1);
+            List2.Add(1);
+            List2.Add(0);
+            List2.Add(1);
+            List2.Add(0);
+            List2.Add(1);
+
+            List<byte> resultList = new List<byte>();
+            resultList.Add(1);
+            resultList.Add(0);
+            resultList.Add(0);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(1);
+            resultList.Add(0);
+            resultList.Add(1);
+            resultList.Add(0);
+            resultList.Add(1);
+
+            Assert.IsTrue(IsListValid(List1), "Invalid List1");
+            Assert.IsTrue(IsListValid(List2), "Invalid List2");
+
+            List<byte> calculateList = List1XorList2(List1, List2);
+
+            Assert.IsTrue(AreEqualLists(resultList, calculateList));
+        }
+
+        [TestMethod]
+        public void XORForListsWithTheSameLength()
+        {
+            List<byte> List1 = new List<byte>();
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(0);
+
+            List<byte> List2 = new List<byte>();
+            List2.Add(0);
+            List2.Add(1);
+            List2.Add(1);
+            List2.Add(0);
+
+            List<byte> resultList = new List<byte>();
+            resultList.Add(1);
+            resultList.Add(0);
+            resultList.Add(1);
+            resultList.Add(0);
+
+            Assert.IsTrue(IsListValid(List1), "Invalid List1");
+            Assert.IsTrue(IsListValid(List2), "Invalid List2");
+
+            List<byte> calculateList = List1XorList2(List1, List2);
+
+            Assert.IsTrue(AreEqualLists(resultList, calculateList));
+        }
+
+        [TestMethod]
+        public void NotForList()
+        {
+            List<byte> List1 = new List<byte>();
+            List1.Add(1);
+            List1.Add(1);
+            List1.Add(0);
+            List1.Add(0);
+
+            List<byte> resultList = new List<byte>();
+            resultList.Add(0);
+            resultList.Add(0);
+            resultList.Add(1);
+            resultList.Add(1);
+
+            Assert.IsTrue(IsListValid(List1), "Invalid List1");
+
+            List<byte> calculateList = NotList(List1);
+
+            Assert.IsTrue(AreEqualLists(resultList, calculateList));
+        }
+
+        private static bool IsListValid(List<byte> listi)
         {
             string good = "01";
             foreach (byte b in listi)
@@ -200,9 +405,73 @@ namespace TipuriPrimitive
             return List3;
         }
 
+        private static List<byte> List1OrList2(List<byte> List1, List<byte> List2)
+        {
+            List<byte> List3 = new List<byte>();
+            for (int i = 0; i < Math.Min(List1.Count, List2.Count); i++)
+            {
+                List3.Add(OrForBytes(List1[i], List2[i]));
+            }
+            if (List1.Count > List2.Count)
+            {
+                List3.AddRange(List1.GetRange(List2.Count, List1.Count - List2.Count));
+
+            }
+            else
+            {
+                List3.AddRange(List2.GetRange(List1.Count, List2.Count - List1.Count));
+            }
+            return List3;
+        }
+
+        private static List<byte> List1XorList2(List<byte> List1, List<byte> List2)
+        {
+            List<byte> List3 = new List<byte>();
+            for (int i = 0; i < Math.Min(List1.Count, List2.Count); i++)
+            {
+                List3.Add(XorForBytes(List1[i], List2[i]));
+            }
+            if (List1.Count > List2.Count)
+            {
+                List3.AddRange(List1.GetRange(List2.Count, List1.Count - List2.Count));
+
+            }
+            else
+            {
+                List3.AddRange(List2.GetRange(List1.Count, List2.Count - List1.Count));
+            }
+            return List3;
+        }
+
+        private static List<byte> NotList(List<byte> List1)
+        {
+            List<byte> List2 = new List<byte>();
+            for (int i = 0; i < List1.Count; i++)
+            {
+                List2.Add(NotForByte(List1[i]));
+            }
+
+            return List2;
+        }
+
         private static byte AndForBytes(byte c1, byte c2)
         {
             return (c1 == c2) ? c1 : Convert.ToByte(0);
+        }
+
+        private static byte OrForBytes(byte c1, byte c2)
+        {
+            return ((c1 == 1) || (c2 == 1)) ? Convert.ToByte(1) : Convert.ToByte(0);
+        }
+
+        private static byte XorForBytes(byte c1, byte c2)
+        {
+            return ((c1 == c2)) ? Convert.ToByte(0) : Convert.ToByte(1);
+        }
+
+        private static byte NotForByte(byte c1)
+        {
+            return ((c1 == 1)) ? Convert.ToByte(0) : Convert.ToByte(1);
         }
 
         private static List<byte> ConvertFromXBaseToYBase(List<byte> inputNumber, int xbase, int ybase)
