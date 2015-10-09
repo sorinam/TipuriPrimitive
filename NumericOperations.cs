188f20b553b17889127fdfa93853265fe1d80a0e
Sorina Mancas <sorinam@yahoo.com>
2015-10-09 15:18:59 +0300

    refactorized: Converting from Decimal to X Base; Converting Fom X Base to Y Base

--- NumericOperations.cs
+++ NumericOperations.cs
@@ -52,12 +52,10 @@ namespace TipuriPrimitive
         public void ConvertFromXBaseToDecimal()
         {
             int baseNumber = 2;
+            byte[] inputNumber = { 0, 1, 0, 1, 0, 0, 1, 1, 1 };
+            int expectedNumber = 458;
 
-            byte[] inputNumber = { 0, 1 };
-
-            int expectedNumber = 2;
-
-            int calculateNumber = ConvertFromXBaseToDecimal(inputNumber, baseNumber);
+            var calculateNumber = ConvertFromXBaseToDecimal(inputNumber, baseNumber);
 
             Assert.AreEqual(expectedNumber, calculateNumber);
         }
@@ -66,11 +64,10 @@ namespace TipuriPrimitive
         public void ConvertFromHexaToDecimal()
         {
             int baseNumber = 16;
-
             byte[] inputNumber = { 14, 5, 2, 12, 7, 4, 13 };
-
             int expectedNumber = 222806622;
-            int calculateNumber = ConvertFromXBaseToDecimal(inputNumber, baseNumber);
+            
+            var calculateNumber = ConvertFromXBaseToDecimal(inputNumber, baseNumber);
 
             Assert.AreEqual(expectedNumber, calculateNumber);
         }
@@ -80,11 +77,9 @@ namespace TipuriPrimitive
         {
             int Xbase = 16;
             int Ybase = 2;
-
             byte[] inputNumber = { 4, 1 };
-
             byte[] expectedNumber = { 0, 0, 1, 0, 1 };
-
+            
             var calculateNumber = ConvertFromXBaseToYBase(inputNumber, Xbase, Ybase);
 
             CollectionAssert.AreEqual(expectedNumber, calculateNumber);
@@ -185,9 +180,9 @@ namespace TipuriPrimitive
         public void NotForList()
         {
             byte[] inputArray = { 1, 1, 0, 0 };
-           
-            byte[] resultArray = { 0,0,1,1};
-          
+
+            byte[] resultArray = { 0, 0, 1, 1 };
+
             Assert.IsTrue(IsListValid(inputArray), "Invalid inputArray");
 
             var calculateArray = NotList(inputArray);
@@ -196,10 +191,10 @@ namespace TipuriPrimitive
         }
 
         private static bool IsListValid(byte[] listi)
-        {           
+        {
             foreach (byte b in listi)
             {
-                if (b!=0 ||b!=1)
+                if (b != 0 || b != 1)
                 {
                     return false;
                 }
@@ -212,9 +207,9 @@ namespace TipuriPrimitive
             byte[] resultArray = new byte[16];
             for (int i = 0; i < Math.Min(List1.Length, List2.Length); i++)
             {
-                resultArray[i]=AndForBytes(List1[i], List2[i]);
+                resultArray[i] = AndForBytes(List1[i], List2[i]);
             }
-            if (List1.Length> List2.Length)
+            if (List1.Length > List2.Length)
             {
                 //resultArray.AddRange(List1.GetRange(List2.Count, List1.Count - List2.Count));
 
@@ -231,7 +226,7 @@ namespace TipuriPrimitive
             byte[] resultArray = new byte[16];
             for (int i = 0; i < Math.Min(List1.Length, List2.Length); i++)
             {
-                resultArray[i]=OrForBytes(List1[i], List2[i]);
+                resultArray[i] = OrForBytes(List1[i], List2[i]);
             }
             if (List1.Length > List2.Length)
             {
@@ -250,7 +245,7 @@ namespace TipuriPrimitive
             byte[] resultArray = new byte[16];
             for (int i = 0; i < Math.Min(List1.Length, List2.Length); i++)
             {
-                resultArray[i]=XorForBytes(List1[i], List2[i]);
+                resultArray[i] = XorForBytes(List1[i], List2[i]);
             }
             if (List1.Length > List2.Length)
             {
@@ -259,7 +254,7 @@ namespace TipuriPrimitive
             }
             else
             {
-               // resultArray.AddRange(List2.GetRange(List1.Count, List2.Count - List1.Count));
+                // resultArray.AddRange(List2.GetRange(List1.Count, List2.Count - List1.Count));
             }
             return resultArray;
         }
@@ -269,7 +264,7 @@ namespace TipuriPrimitive
             byte[] resultList = new byte[16];
             for (int i = 0; i < List1.Length; i++)
             {
-                resultList[i]=NotForByte(List1[i]);
+                resultList[i] = NotForByte(List1[i]);
             }
 
             return resultList;
@@ -318,7 +313,7 @@ namespace TipuriPrimitive
         private static int CalculateNoFromList(byte[] inputNumber)
         {
             int result = 0;
-            for (int i = 0; i <= inputNumber.Length; i++)
+            for (int i = 0; i < inputNumber.Length; i++)
             {
                 result += inputNumber[i] * (int)Math.Pow(10, i);
             }
@@ -328,11 +323,10 @@ namespace TipuriPrimitive
         private static int ConvertFromXBaseToDecimal(byte[] inputNumber, int baseNumber)
         {
             int result = 0;
-            int value = 0;
             for (int i = 0; i < inputNumber.Length; i++)
             {
-                      result += value * (int)Math.Pow(baseNumber, i);
-             }
+                result += inputNumber[i] * (int)Math.Pow(baseNumber, i);
+            }
             return result;
         }
 
