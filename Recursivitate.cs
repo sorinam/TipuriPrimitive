33e2ad2f15016da10c021ce59dfdb981819e315f
Sorina Mancas <sorinam@yahoo.com>
2015-10-13 11:56:09 +0300

    add ReverseString test

---
+++ Recursivitate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TipuriPrimitive
+{
+	[TestClass]
+	public class Recursivitate
+	{
+		[TestMethod]
+		public void ReverseString()
+		{
+            String  inputString = "mama are mere";
+            String outputString = "erem era amam";
+            var calculateString = ReverseString(inputString);
+            Assert.AreEqual(calculateString, outputString);
+		}
+
+        private String ReverseString(string inputString)
+        {
+            if (inputString.Length > 0)
+                return inputString[inputString.Length - 1] + ReverseString(inputString.Substring(0, inputString.Length - 1));
+            else
+                return inputString;            
+        }
+    }
+}
