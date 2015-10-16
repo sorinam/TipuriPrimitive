254aecf52c169591a2b59583514849c329a9e818
Sorina Mancas <sorinam@yahoo.com>
2015-10-16 07:12:24 +0300

    refactorized Hanoi test

--- HanoiTowers.cs
+++ HanoiTowers.cs
@@ -7,27 +7,35 @@ namespace TipuriPrimitive
     [TestClass]
     public class HanoiTowers
     {
-        public List<string> listOfMovements = new List<string>();
+
+        public static int numberofDisks = 2;
+        public string[,] moves = new string [(int)Math.Pow(2,4)-1,3];
+        public int movesCount = 0;
 
         [TestMethod]
         public void TowersofHanoi()
-        {
-            int numberofDisks = 20;
+        {   
             Hanoi(numberofDisks, 'A', 'B', 'C');
-            Assert.AreEqual(1048575, listOfMovements.Count);
+            Assert.AreEqual(104, movesCount);
         }
-        private void Hanoi(int n, char source, char destination, char via)
-        {  if (n==0)
+        private void Hanoi(int numberOfDisk, char source, char destination, char via)
+        {  if (numberOfDisk==0)
             {
                 return;
             }
-            if (n > 0)
+            if (numberOfDisk > 0)
             {
-                Hanoi(n - 1, source, via, destination);
-                string movement="Move disk "+n+" from disk "+source+" to disk " + destination;
-                listOfMovements.Add(movement);
-                Hanoi(n - 1, via, destination, source);
+                Hanoi(numberOfDisk - 1, source, via, destination);
+                SaveMove(numberOfDisk,source,destination);
+                Hanoi(numberOfDisk - 1, via, destination, source);
             }
+        }
+        public void SaveMove(int n, char source, char destination)
+        {
+            moves[movesCount, 0] = n.ToString();
+            moves[movesCount, 1] = source.ToString();
+            moves[movesCount, 2] = destination.ToString();
+            movesCount++;
         }
     }
 }
