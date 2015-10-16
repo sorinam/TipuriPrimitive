9da674dbf4452fe472f11a5ca3639822c319da8f
Sorina Mancas <sorinam@yahoo.com>
2015-10-16 09:34:44 +0300

    add a method to move disk from source to destination list

--- HanoiTowers.cs
+++ HanoiTowers.cs
@@ -8,25 +8,29 @@ namespace TipuriPrimitive
     public class HanoiTowers
     {
 
-        public static int numberofDisks = 2;
-        public string[,] moves = new string [(int)Math.Pow(2,4)-1,3];
+        public int numberofDisks = 4;
+        public string[,] moves = new string[(int)Math.Pow(2, 4) - 1, 3];
         public int movesCount = 0;
+        public List<int> ListA = new List<int> {4,3,2,1};
+        public List<int> ListB = new List<int>();
+        public List<int> ListC = new List<int>();
 
         [TestMethod]
         public void TowersofHanoi()
-        {   
+        {
             Hanoi(numberofDisks, 'A', 'B', 'C');
-            Assert.AreEqual(104, movesCount);
+            Assert.AreEqual(16, movesCount);
         }
         private void Hanoi(int numberOfDisk, char source, char destination, char via)
-        {  if (numberOfDisk==0)
+        {
+            if (numberOfDisk == 0)
             {
                 return;
             }
             if (numberOfDisk > 0)
             {
                 Hanoi(numberOfDisk - 1, source, via, destination);
-                SaveMove(numberOfDisk,source,destination);
+                SaveMove(numberOfDisk, source, destination);
                 Hanoi(numberOfDisk - 1, via, destination, source);
             }
         }
@@ -35,7 +39,31 @@ namespace TipuriPrimitive
             moves[movesCount, 0] = n.ToString();
             moves[movesCount, 1] = source.ToString();
             moves[movesCount, 2] = destination.ToString();
+            MoveDisk(n, source, destination);
             movesCount++;
+        }
+
+        private void MoveDisk(int n, char from, char to)
+        {
+            List<int> Source = GetList(from);
+            List<int> Destination = GetList(to);
+            Source.Remove(n);
+            Destination.Add(n);
+           
+        }
+
+        private List<int> GetList(char nameofList)
+        {
+            switch (nameofList)
+            {
+                case 'A':
+                    return ListA;
+                case 'B':
+                    return ListB;
+                case 'C':
+                    return ListC;
+            }
+            return ListA;
         }
     }
 }
