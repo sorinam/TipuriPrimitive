65f969ac083b228edb6e10494caf4c8d2becbfd8
Sorina Mancas <sorinam@yahoo.com>
2015-10-16 15:00:34 +0300

    refactorized remove SaveMoves method

--- HanoiTowers.cs
+++ HanoiTowers.cs
@@ -7,68 +7,97 @@ namespace TipuriPrimitive
     [TestClass]
     public class HanoiTowers
     {
-
-        public int numberofDisks =10;
-        public string[,] moves = new string[(int)Math.Pow(2, 10) - 1, 3];
         public Int64 movesCount = 0;
-        public List<int> ATower = new List<int> {10,9,8,7,6,5,4,3,2,1};
-        public List<int> BTower = new List<int>();
-        public List<int> CTower = new List<int>();
 
         [TestMethod]
         public void TowersofHanoi()
         {
-            Hanoi(numberofDisks, 'A', 'B', 'C');
-            Assert.AreEqual(16343, movesCount);
+            int numberOfDisks = 20;
+            Int64 expectedMoves = (Int64)Math.Pow(2, numberOfDisks) - 1;
+
+            List<int> ATower = GenerateDisks(numberOfDisks);
+            List<int> BTower = new List<int>();
+            List<int> CTower = new List<int>();
+
+            HanoiTowersMoves(numberOfDisks, ATower, BTower, CTower);
+
+            Assert.AreEqual(expectedMoves, movesCount);
         }
-        private void Hanoi(int numberOfDisk, char source, char destination, char via)
+
+        [TestMethod]
+        public void TowersofHanoiOneDisk()
         {
-            if (numberOfDisk == 0)
+            int numberOfDisks = 1;
+          
+            List<int> ATower = GenerateDisks(numberOfDisks);
+            List<int> BTower = new List<int>();
+            List<int> CTower = new List<int>();
+
+            HanoiTowersMoves(numberOfDisks, ATower, BTower, CTower);
+
+            Assert.AreEqual(1, movesCount);
+        }
+
+        [TestMethod]
+        public void TowersofHanoiTwoDisks()
+        {
+            int numberOfDisks = 2;
+
+            List<int> ATower = GenerateDisks(numberOfDisks);
+            List<int> BTower = new List<int>();
+            List<int> CTower = new List<int>();
+
+            HanoiTowersMoves(numberOfDisks, ATower, BTower, CTower);
+
+            Assert.AreEqual(3, movesCount);
+        }
+
+        [TestMethod]
+        public void TowersofHanoiThreeDisks()
+        {
+            int numberOfDisks = 3;
+
+            List<int> ATower = GenerateDisks(numberOfDisks);
+            List<int> BTower = new List<int>();
+            List<int> CTower = new List<int>();
+
+            HanoiTowersMoves(numberOfDisks, ATower, BTower, CTower);
+
+            Assert.AreEqual(7, movesCount);
+        }
+
+        private List<int> GenerateDisks(int index)
+        {
+            List<int> outputList = new List<int>();
+            for (int i = 0; i < index; i++)
+            {
+                outputList.Add(index - i);
+            }
+            return outputList;
+        }
+
+        private void HanoiTowersMoves(int n, List<int> source, List<int> destination, List<int> via)
+        {
+            if (n == 0)
             {
                 return;
             }
-            if (numberOfDisk > 0)
+            if (n > 0)
             {
-                Hanoi(numberOfDisk - 1, source, via, destination);
-                SaveMove(numberOfDisk, source, destination);
-                MoveDisk(numberOfDisk, source, destination);
+                HanoiTowersMoves(n - 1, source, via, destination);
+                MoveDisk(n, source, destination);
                 movesCount++;
-                Hanoi(numberOfDisk - 1, via, destination, source);
+                HanoiTowersMoves(n - 1, via, destination, source);
             }
         }
-        public void SaveMove(int n, char source, char destination)
+
+        private void MoveDisk(int n, List<int> source, List<int> destination)
         {
-            moves[movesCount, 0] = n.ToString();
-            moves[movesCount, 1] = source.ToString();
-            moves[movesCount, 2] = destination.ToString();
-            
+            var index = source.Count - 1;
+            int topDisk = source[index];
+            source.Remove(topDisk);
+            destination.Add(topDisk);
         }
 
-        private void MoveDisk(int n, char from, char to)
-        {
-            List<int> Source = GetList(from);
-            List<int> Destination = GetList(to);
-            Source.Remove(n);
-            Destination.Add(n);
-           
-        }
-
-        private List<int> GetList(char nameofList)
-        {
-            List<int> result = new List<int>();
-            switch (nameofList)
-            {
-                case 'A':
-                    result = ATower;
-                    break;
-                case 'B':
-                    result = BTower;
-                    break;
-                case 'C':
-                    result = CTower;
-                    break;
-            }
-            return result; ;
-        }
     }
 }
