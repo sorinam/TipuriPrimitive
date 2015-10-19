c987df47ac2245cd78475d67d3cd68e7bd8739c7
Sorina Mancas <sorinam@yahoo.com>
2015-10-19 21:33:36 +0300

    add validation for Loto numbers, Add Selection Sort Method

--- Sorting.cs
+++ Sorting.cs
@@ -8,22 +8,88 @@ namespace TipuriPrimitive
     public class Sorting
     {
         [TestMethod]
-        public void DisplayLotoNumbers()
+        public void DisplayLotoNumbersUsingMergeSortAlghorithm()
         {
             int[] extractedLotoNumbers = { 16, 8, 1, 15, 46, 3 };
             int[] sortedLotoNumbers = { 1, 3, 8, 15, 16, 46 };
+
+            Assert.IsTrue(IsValid(extractedLotoNumbers),"Invalid array !");
             MergeSort(ref extractedLotoNumbers);
             CollectionAssert.AreEqual(sortedLotoNumbers, extractedLotoNumbers);
         }
 
+        private bool IsValid(int[] LotoNumbers)
+        {
+            if (LotoNumbers.Length < 1) return false;
+            for (int i = 0; i < LotoNumbers.Length -1; i++)
+            {
+                if (IsFound(LotoNumbers,LotoNumbers[i],i)) return false;
+            }
+            return true;
+        }
+
+        private bool IsFound(int[] numbers, int value, int index)
+        {
+          for (int i=index+1; i<numbers.Length;i++)
+            {
+                if (numbers[i] == value) return true;
+            }
+            return false;
+        }
+
         [TestMethod]
-        public void DisplayLotoNoNumbers()
+        public void DisplayLotoNoNumbersUsingMergeSortAlghoritm()
         {
             int[] extractedLotoNumbers = { };
             int[] sortedLotoNumbers = { };
             MergeSort(ref extractedLotoNumbers);
             CollectionAssert.AreEqual(sortedLotoNumbers, extractedLotoNumbers);
         }
+
+        [TestMethod]
+        public void TestArrayMergeSortAlghoritm()
+        {
+            int[] sourceArray = { 2 };
+            int[] sortedArray = { 2 };
+            MergeSort(ref sourceArray);
+            CollectionAssert.AreEqual(sortedArray, sourceArray);
+        }
+        [TestMethod]
+        public void TestArrayWithTwoElementsMergeSortAlghoritm()
+        {
+            int[] sourceArray = { 12, 8 };
+            int[] sortedArray = { 8, 12 };
+            MergeSort(ref sourceArray);
+            CollectionAssert.AreEqual(sortedArray, sourceArray);
+        }
+
+        [TestMethod]
+        public void TestArrayWithThreeElementsMergeSortAlghoritm()
+        {
+            int[] sourceArray = { 12, 1, 8 };
+            int[] sortedArray = { 1, 8, 12 };
+            MergeSort(ref sourceArray);
+            CollectionAssert.AreEqual(sortedArray, sourceArray);
+        }
+
+        [TestMethod]
+        public void DisplayLotoNumbersUsingSelectionAlghorithm()
+        {
+            int[] extractedLotoNumbers = { 16, 8, 1, 15, 46, 3 };
+            int[] sortedLotoNumbers = { 1, 3, 8, 15, 16, 46 };
+            SelectionSort(ref extractedLotoNumbers);
+            CollectionAssert.AreEqual(sortedLotoNumbers, extractedLotoNumbers);
+        }
+
+        [TestMethod]
+        public void DisplayLotoNoNumbersUsingSelectionAlghorithm()
+        {
+            int[] extractedLotoNumbers = { };
+            int[] sortedLotoNumbers = { };
+            SelectionSort(ref extractedLotoNumbers);
+            CollectionAssert.AreEqual(sortedLotoNumbers, extractedLotoNumbers);
+        }
+
         [TestMethod]
         public void MergeSortedArrays()
         {
@@ -37,23 +103,26 @@ namespace TipuriPrimitive
 
         private static void MergeSort(ref int[] inputArray)
         {
-            if (inputArray.Length <= 2)
+            if (inputArray.Length > 0)
             {
-                if (inputArray.Length == 1) return;
-                if (inputArray[0] > inputArray[1])
+                if (inputArray.Length <= 2)
                 {
-                    swap(ref inputArray[0], ref inputArray[1]);
-                    return;
+                    if (inputArray.Length == 1) return;
+                    if (inputArray[0] > inputArray[1])
+                    {
+                        Swap(ref inputArray[0], ref inputArray[1]);
+                        return;
+                    }
                 }
+                int index = inputArray.Length / 2;
+                int[] leftArray = new int[index];
+                int[] rightArray = new int[inputArray.Length - index];
+                Array.Copy(inputArray, leftArray, index);
+                Array.Copy(inputArray, index, rightArray, 0, inputArray.Length - index);
+                MergeSort(ref leftArray);
+                MergeSort(ref rightArray);
+                Merge(leftArray, rightArray, ref inputArray);
             }
-            int index = inputArray.Length / 2;
-            int[] leftArray = new int[index];
-            int[] rightArray = new int[inputArray.Length - index];
-            Array.Copy(inputArray, leftArray, index);
-            Array.Copy(inputArray, index, rightArray, 0, inputArray.Length - index);
-            MergeSort(ref leftArray);
-            MergeSort(ref rightArray);
-            Merge(leftArray, rightArray, ref inputArray);
         }
 
         private static void Merge(int[] firstArray, int[] secondArray, ref int[] sortedArray)
@@ -85,11 +154,31 @@ namespace TipuriPrimitive
             }
         }
 
-        private static void swap(ref int first, ref int second)
+        private static void SelectionSort(ref int[] sourceArray)
         {
-            var temporary = first;
-            first = second;
-            second = temporary;
+            int indexOfMinim;
+            for (int i = 0; i < sourceArray.Length - 1; i++)
+            {
+                indexOfMinim = i;
+                for (int j = i + 1; j < sourceArray.Length; j++)
+                {
+                    if (sourceArray[j] < sourceArray[indexOfMinim])
+                    {
+                        indexOfMinim = j;
+                    }
+                }
+                if (indexOfMinim != i)
+                {
+                    Swap(ref sourceArray[i], ref sourceArray[indexOfMinim]);
+                }
+            }
+        }
+
+        private static void Swap(ref int firstElement, ref int secondElement)
+        {
+            var temporary = firstElement;
+            firstElement = secondElement;
+            secondElement = temporary;
         }
     }
 }
