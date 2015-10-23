d575c36d60ebb084054c894736d1f1e869622604
Sorina Mancas <sorinam@yahoo.com>
2015-10-23 16:18:50 +0300

    add test to find a student with a custom grade

--- Catalogue.cs
+++ Catalogue.cs
@@ -11,8 +11,8 @@ namespace TipuriPrimitive
         public struct Student
         {
             public string name;
-            public float finalGrade;
-            public Student(string name, float grade)
+            public decimal finalGrade;
+            public Student(string name, decimal grade)
             {
                 this.name = name;
                 this.finalGrade = grade;
@@ -34,7 +34,7 @@ namespace TipuriPrimitive
         }
 
         [TestMethod]
-        public void Students()
+        public void SortDescendingStudents()
         {
             StudentGrades[] Catalogue =
                {
@@ -47,10 +47,67 @@ namespace TipuriPrimitive
             new StudentGrades("Vladimir Mihai",new uint[]{ 8,10, 9,10 }, new uint[]{9,10,10},new uint[] {10,7,8 }),
             new StudentGrades("Popescu Mihai",new uint[]{5,8,10, 9 ,9}, new uint[]{9,4,10,10},new uint[] {10,7,4,8 }),
                 };
+            
+            Student[] expectedSortedList =
+            {
+                new Student ("Ban Andrei",9.14M),
+                new Student ("Vladimir Mihai",9.08M),
+                new Student ("Vlad Mihai",9.06M),
+                new Student ("Bancos Andrei",8.92M),
+                new Student ("Andrei Maria",8.67M),
+                new Student ("Man Maria",8.22M),
+                new Student ("Popescu Mihai",7.9M),
+                new Student ("Pop Mihai",7.75M),
+            };
+
             Student[] StudentsGrades = CalculateGradesForAllStudents(Catalogue);
             InsertionSortGeneralGrades(ref StudentsGrades);
-            Assert.AreEqual(1, 3);
 
+            CollectionAssert.AreEqual(expectedSortedList,StudentsGrades);
+        }
+
+        [TestMethod]
+        public void FindStudentWithSpecifiedGrades()
+        {
+            StudentGrades[] Catalogue =
+               {
+            new StudentGrades ("Andrei Maria",new uint[] { 10,9,9},new uint[] {8 },new uint[] {9,10,7 }),
+            new StudentGrades("Ban Andrei",new uint[]{ 10, 10, 9 }, new uint[]{10,9,10,10},new uint[] {9,7 }),
+            new StudentGrades("Vlad Mihai",new uint[]{ 8,10, 9 }, new uint[]{9,10,10},new uint[] {10,7 }),
+            new StudentGrades("Pop Mihai",new uint[]{5,8,10, 9 }, new uint[]{9,4,10,10},new uint[] {10,7,4 }),
+            new StudentGrades ("Man Maria",new uint[] { 10,9},new uint[] {8 ,5},new uint[] {9,10,7 }),
+            new StudentGrades("Bancos Andrei",new uint[]{ 10, 10 }, new uint[]{10,9,10,10},new uint[] {9,7,5 }),
+            new StudentGrades("Vladimir Mihai",new uint[]{ 8,10, 9,10 }, new uint[]{9,10,10},new uint[] {10,7,8 }),
+            new StudentGrades("Popescu Mihai",new uint[]{5,8,10, 9 ,9}, new uint[]{9,4,10,10},new uint[] {10,7,4,8 }),
+                };
+            decimal generalGradeToFind = 8.67M;
+            string expectedStudent = "Andrei Maria";
+
+            Student[] StudentsGrades = CalculateGradesForAllStudents(Catalogue);
+            InsertionSortGeneralGrades(ref StudentsGrades);
+            string studentFound = BinarySearch(StudentsGrades, generalGradeToFind, 0, StudentsGrades.Length - 1);
+
+            Assert.AreEqual(expectedStudent,studentFound);
+        }
+
+        private static string BinarySearch(Student[] studentsGrades, decimal GradeToFind,int begin,int end)
+        {
+            if (begin > end) return null;
+            int middle = begin + (end - begin) / 2;
+            Student middleElement = studentsGrades[middle];
+            if (GradeToFind<middleElement.finalGrade)
+            {
+               return  BinarySearch(studentsGrades, GradeToFind, middle + 1, end);
+            }
+            else
+            {
+                if (GradeToFind == middleElement.finalGrade)
+                {
+                    return middleElement.name;
+                }
+            }
+            return BinarySearch(studentsGrades, GradeToFind, begin, middle - 1);
+            
         }
 
         private void InsertionSortGeneralGrades(ref Student[] studentsGrades)
@@ -101,14 +158,14 @@ namespace TipuriPrimitive
         private static void CalculateGradeForAStudent(StudentGrades[] catalogue, int i, ref Student[] studentsGrades)
         {
             studentsGrades[i].name = catalogue[i].name;
-            float generalGrade = (CalculateGrade(catalogue[i].Discipline1Grades) + CalculateGrade(catalogue[i].Discipline2Grades) + CalculateGrade(catalogue[i].Discipline3Grades)) / 3;
-            studentsGrades[i].finalGrade = generalGrade;
+            decimal generalGrade = (CalculateGrade(catalogue[i].Discipline1Grades) + CalculateGrade(catalogue[i].Discipline2Grades) + CalculateGrade(catalogue[i].Discipline3Grades)) / 3;
+            studentsGrades[i].finalGrade = Math.Round(generalGrade,2);
         }
 
-        private static float CalculateGrade(uint[] grades)
+        private static decimal CalculateGrade(uint[] grades)
         {
             if (grades.Length == 0) return 0;
-            float sum = 0;
+            decimal sum = 0;
             for (int i = 0; i < grades.Length; i++)
             {
                 sum += grades[i];
